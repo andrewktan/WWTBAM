96d3013a9bf6a3a71c6724933cefc334bc6405a1
=Noor Gangi <noorgangi95@gmail.com>
2014-04-16 11:16:24 -0400

    Starting main screen class

---
+++ src/MainScreen.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.awt.event.WindowFocusListener;
+
+public class MainScreen extends JFrame implements ActionListener,WindowListener,WindowFocusListener
+{
+    private BufferedImage titlePage;
+    private JButton startBtn = new JButton ("Start");
+    private JButton instructionsBtn = new JButton ("
+    
+}
+
+
+
+
