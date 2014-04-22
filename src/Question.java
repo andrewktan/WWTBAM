0d5232a9fa228f405535c6fbcd5fbdeed0c9b26b
=Noor Gangi <noorgangi95@gmail.com>
2014-04-22 12:02:52 -0400

    Hey

--- src/Question.java
+++ src/Question.java
@@ -3,9 +3,10 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-public class Question {
+public class Question
+{
     protected String prompt;
-    protected ArrayList<String> answers;
+    protected ArrayList < String > answers;
     protected int correct;
 
     /**
@@ -14,52 +15,59 @@ public class Question {
      * @param answers
      * @param correct
      */
-    public Question(String prompt, ArrayList<String> answers, int correct) {
+    public Question (String prompt, ArrayList < String > answers, int correct)
+    {
 
-        this.prompt = prompt;
+	this.prompt = prompt;
 
-        // randomize answer order
-        String correctAns = answers.get(correct);
-        Collections.shuffle(answers);
-        this.answers = answers;
+	// randomize answer order
+	String correctAns = answers.get (correct);
+	Collections.shuffle (answers);
+	this.answers = answers;
 
-        for (int i=0; i<4; i++) // find the correct answer
-            if (answers.get(i) == correctAns)
-                this.correct = i;
+	for (int i = 0 ; i < 4 ; i++) // find the correct answer
+	    if (answers.get (i) == correctAns)
+		this.correct = i;
     }
+
 
     /**
      * accessor for prompt
      * @return prompt
      */
-    public String getPrompt() {
-        return prompt;
+    public String getPrompt ()
+    {
+	return prompt;
     }
+
 
     /**
      * accessor for answers
      * @return four answers including the correct one in random order
      */
-    public ArrayList<String> getAnswers() {
-        return answers;
+    public ArrayList < String > getAnswers ()
+    {
+	return answers;
     }
+
 
     /**
      *
      * @return
      */
-    public int getCorrect() {
-        return correct;
+    public int getCorrect ()
+    {
+	return correct;
     }
+
 
     /**
      * returns true if correct answer is given
      * @param x answer
      * @return if correct
      */
-    public boolean isCorrect(int x) {
-        return x == correct;
+    public boolean isCorrect (int x)
+    {
+	return x == correct;
     }
-
-
 }
