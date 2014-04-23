379cedca945993c827e395ddf0a134da91fc5529
=Noor Gangi <noorgangi95@gmail.com>
2014-04-23 10:35:19 -0400

    asjd;flkadjas

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -6,11 +6,12 @@ import java.io.IOException;
 import java.awt.image.BufferedImage;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.util.ArrayList;
 
 public class Gameplay extends JFrame implements ActionListener, WindowListener
 {
     //Four main choices buttons
-    ArrayList < JButton > choiceList = new ArrayList < JButton > ();
+    private ArrayList< JButton > choiceList = new ArrayList < JButton > ();
 
     //Visual for the question box
     private BufferedImage questionBox;
@@ -19,23 +20,28 @@ public class Gameplay extends JFrame implements ActionListener, WindowListener
     private String player;
 
     //Questions & Answers
-    ArrayList < Question > questions;
-    ArrayList < String > answers;
+    private ArrayList < Question > questions;
+    private ArrayList < String > answers;
 
     //Score
     private int score;
 
     //Menu
-    private JMenuBar mainMENU = new JMenuBar ();
-    private JMenu InstructionsMENU = new JMenu ("Information");
-    private JMenuItem newMENU = new JMenuItem ("New Game");
-
-    this.questions = QuestionReader.readQuestionsFromFile ("questions.xml");
+    private JMenuBar menuBar = new JMenuBar ();
+    private JMenu GameMENU = new JMenu ("Game");
+    menuBar.Aadd(GameMENU);
+    private JMenuItem newGameITEM = new JMenuItem ("New Game");
+    GameMENU.add(newGameITEM);
 
     public Gameplay ()
     {
+<<<<<<< HEAD
 
-	Question currentQuestion = questions.get ((int) (Math.random () * questions.size ()) + 1));
+	Question currentQuestion = questions.remove ((int) (Math.random () * questions.size () + 1));
+=======
+    this.questions = QuestionReader.readQuestionsFromFile ("questions.xml");
+	Question currentQuestion = questions.get ((int) (Math.random () * questions.size ()) + 1);
+>>>>>>> 87d981f9e87eb6ea2bdbfec83839016d3e8e6d4c
 	answers = currentQuestion.getAnswers ();
 
 	try
@@ -47,16 +53,22 @@ public class Gameplay extends JFrame implements ActionListener, WindowListener
 
 	}
 
-	catch (IOException e)
+<<<<<<< HEAD
+	catch (IOException e)  //Just in case
+=======
+	catch (Exception e)
+>>>>>>> 87d981f9e87eb6ea2bdbfec83839016d3e8e6d4c
 	{
 
 	}
 
 
-
-
     }
 
+    // for ActionListener interface
+    public void actionPerformed (ActionEvent e)
+    {
+    }
 
     // Method that must be implemented because of Window Listener, does nothing
     public void windowDeactivated (WindowEvent e)
@@ -87,6 +99,10 @@ public class Gameplay extends JFrame implements ActionListener, WindowListener
     {
     }
 
+    // Method that must be implemented because of Window Listener, does nothing
+    public void windowClosing (WindowEvent e)
+    {
+    }
 
     // Method that must be implemented because of Window Listener, does nothing
     public void windowActivated (WindowEvent e)
--- src/MainScreen.java
+++ src/MainScreen.java
@@ -4,40 +4,43 @@ import javax.swing.*;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.io.IOException;
+import java.io.IOException;git o
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.awt.event.WindowFocusListener;
 
-public class MainScreen extends JFrame implements ActionListener,WindowListener,WindowFocusListener
+public class MainScreen extends JFrame implements ActionListener, WindowListener, WindowFocusListener
 {
     private BufferedImage titlePage;
     private JButton startBtn = new JButton ("Start");
     private JButton instructionsBtn = new JButton ("Instructions");
     private JButton quitBtn = new JButton ("Quit");
 
-    public MainScreen() {
-        setTitle("WWTBAM");
-        setSize(800, 450); // default size is 0,0
-        setDefaultLookAndFeelDecorated(true);
-        setLocationRelativeTo(null);
+    public MainScreen ()
+    {
+	setTitle ("WWTBAM");
+	setSize (800, 450); // default size is 0,0
+	setDefaultLookAndFeelDecorated (true);
+	setLocationRelativeTo (null);
 
-        JPanel p = new JPanel();
-        // populate JFrame
+	JPanel p = new JPanel ();
+	// populate JFrame
 
-        p.add(startBtn);
-        p.add(instructionsBtn);
-        p.add(quitBtn);
+	p.add (startBtn);
+	p.add (instructionsBtn);
+	p.add (quitBtn);
 
-        this.setContentPane(p);
+	this.setContentPane (p);
     }
+
 
     // For ActionListener interface
     public void actionPerformed (ActionEvent e)
     {
     }
+
 
     // Method that must be implemented because of Window Listener, does nothing
     public void windowDeactivated (WindowEvent e)
@@ -68,6 +71,7 @@ public class MainScreen extends JFrame implements ActionListener,WindowListener,
     {
     }
 
+
     // For WindowListener interface
     public void windowClosing (WindowEvent e)
     {
@@ -79,16 +83,22 @@ public class MainScreen extends JFrame implements ActionListener,WindowListener,
     {
     }
 
+
     // For WindowFocusListener interface
     public void windowGainedFocus (WindowEvent e)
     {
     }
+
 
     // For WindowFocusListener interface
     public void windowLostFocus (WindowEvent e)
     {
     }
     
+    public void main (String[] args)
+    {
+	
+    }
 }
 
 
