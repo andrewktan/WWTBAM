2d35a41934d4ad9ba7477794ac80fc5950497ce3
Andrew Tan <andrewtsoc@gmail.com>
2014-05-06 00:13:05 -0400

    Added DTD in questions.xml

--- src/Question.java
+++ src/Question.java
@@ -5,125 +5,112 @@ import org.dom4j.io.SAXReader;
 import java.io.FileInputStream;
 import java.util.*;
 
-public class Question
-{
+public class Question {
     protected String prompt;
-    protected ArrayList < String > answers;
+    protected ArrayList<String> answers;
     protected int correct;
 
     /**
      * create a Question object
+     *
      * @param prompt
      * @param answers
      * @param correct
      */
-    public Question (String prompt, ArrayList < String > answers, int correct)
-    {
+    public Question(String prompt, ArrayList<String> answers, int correct) {
 
-	this.prompt = prompt;
+        this.prompt = prompt;
 
-	// randomize answer order
-	String correctAns = answers.get (correct);
-	Collections.shuffle (answers);
-	this.answers = answers;
+        // randomize answer order
+        String correctAns = answers.get(correct);
+        Collections.shuffle(answers);
+        this.answers = answers;
 
-	for (int i = 0 ; i < 4 ; i++) // find the correct answer
-	    if (answers.get (i) == correctAns)
-		this.correct = i;
+        for (int i = 0; i < 4; i++) // find the correct answer
+            if (answers.get(i) == correctAns)
+                this.correct = i;
     }
-
-
-    /**
-     * accessor for prompt
-     * @return prompt
-     */
-    public String getPrompt ()
-    {
-	return prompt;
-    }
-
-
-    /**
-     * accessor for answers
-     * @return four answers including the correct one in random order
-     */
-    public ArrayList < String > getAnswers ()
-    {
-	return answers;
-    }
-
-
-    /**
-     *
-     * @return
-     */
-    public int getCorrect ()
-    {
-	return correct;
-    }
-
-
-    /**
-     * returns true if correct answer is given
-     * @param x answer
-     * @return if correct
-     */
-    public boolean isCorrect (int x)
-    {
-	return x == correct;
-    }
-
 
     /**
      * Read from file
+     *
      * @param str
      * @return
      */
-    public static ArrayList < Question > readQuestionsFromFile (String str)
-    {
+    public static ArrayList<Question> readQuestionsFromFile(String str) {
         // create ArrayList of questions
-        ArrayList < Question > ret = new ArrayList < Question > ();
+        ArrayList<Question> ret = new ArrayList<Question>();
 
-        try
-        {
+        try {
             // create Document object
-            SAXReader reader = new SAXReader ();
-            Document document = reader.read (new FileInputStream("data/" + str));
+            SAXReader reader = new SAXReader();
+            Document document = reader.read(new FileInputStream("data/" + str));
 
             // read data
-            Element root = document.getRootElement ();
-            List questions = root.selectNodes ("/questions/question"); // get list of questions
+            Element root = document.getRootElement();
+            List questions = root.selectNodes("/questions/question"); // get list of questions
 
             // iterate through questions
-            for (Iterator q = questions.iterator () ; q.hasNext () ;)
-            {
-                Element question = (Element) q.next ();
+            for (Iterator q = questions.iterator(); q.hasNext(); ) {
+                Element question = (Element) q.next();
 
                 // get prompt
-                String prompt = ((Element) question.selectNodes ("prompt").get (0)).getText ();
+                String prompt = ((Element) question.selectNodes("prompt").get(0)).getText();
 
                 // get answers
-                ArrayList < String > answers = new ArrayList < String > ();
+                ArrayList<String> answers = new ArrayList<String>();
                 int correct = 0;
-                for (Iterator a = question.selectNodes ("answer").iterator () ; a.hasNext () ;)
-                {
-                    Element answer = (Element) a.next ();
-                    answers.add (answer.getText ());
+                for (Iterator a = question.selectNodes("answer").iterator(); a.hasNext(); ) {
+                    Element answer = (Element) a.next();
+                    answers.add(answer.getText());
 
                     // get correct
-                    if (answer.attribute ("correct") != null)
-                        correct = answers.size () - 1;
+                    if (answer.attribute("correct") != null)
+                        correct = answers.size() - 1;
                 }
 
                 // create question object
-                ret.add (new Question (prompt, answers, correct));
+                ret.add(new Question(prompt, answers, correct));
             }
 
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace ();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return ret;
+    }
+
+    /**
+     * accessor for prompt
+     *
+     * @return prompt
+     */
+    public String getPrompt() {
+        return prompt;
+    }
+
+    /**
+     * accessor for answers
+     *
+     * @return four answers including the correct one in random order
+     */
+    public ArrayList<String> getAnswers() {
+        return answers;
+    }
+
+    /**
+     * @return
+     */
+    public int getCorrect() {
+        return correct;
+    }
+
+    /**
+     * returns true if correct answer is given
+     *
+     * @param x answer
+     * @return if correct
+     */
+    public boolean isCorrect(int x) {
+        return x == correct;
     }
 }
